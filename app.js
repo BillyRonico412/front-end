35cc4e4801b7debe2f4281124c19702885b3c9bb
Billy Ronico <billyronico412@gmail.com>
2021-01-26 16:52:52 +0400

    Ajout du portfolio

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express()
 app.use('/front-end', express.static(__dirname + '/views'))
 app.use(require('morgan')('dev'))
 
+app.get('/', (req, res) => {
+    res.redirect('/front-end/portfolio-bootstrap')
+})
+
 app.get('/favicon.ico', (req, res) => {
     res.sendFile(__dirname + '/favicon.ico')
 })
